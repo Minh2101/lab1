ec6c5fc6c9076d3c14371e62914c964717d6428d
Minh <nbminh2101@gmail.com>
2023-03-15 13:48:36 +0700

    add find, delete item collection

--- collections/item.go
+++ collections/item.go
@@ -18,7 +18,9 @@ type Item struct {
 	Status     bool               `bson:"status" json:"status"`
 	CreatedAt  time.Time          `bson:"created_at" json:"created_at"`
 	ModifiedAt time.Time          `bson:"modified_at" json:"modified_at"`
+	DeletedAt  *time.Time         `bson:"deleted_at" json:"deleted_at"`
 }
+type Items []Item
 
 func (u *Item) CollectionName() string {
 	return "items"
@@ -46,6 +48,41 @@ func (u *Item) Update(DB *mongo.Database) error {
 	if _, err := DB.Collection(u.CollectionName()).UpdateOne(ctx, bson.M{"_id": u.ID}, bson.M{
 		"$set": u,
 	}, options.Update()); err != nil {
+		return err
+	} else {
+		return nil
+	}
+}
+
+func (u *Item) Find(DB *mongo.Database, filter bson.M, opts ...*options.FindOptions) (Items, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), database.CTimeOut)
+	defer cancel()
+	data := make(Items, 0)
+
+	/* Lấy danh sách bản ghi */
+	if cursor, err := DB.Collection(u.CollectionName()).Find(ctx, filter, opts...); err == nil {
+		for cursor.Next(ctx) {
+			var elem Item
+			if err = cursor.Decode(&elem); err != nil {
+				return data, err
+			}
+			data = append(data, elem)
+		}
+		if err = cursor.Err(); err != nil {
+			return data, err
+		}
+		return data, cursor.Close(ctx)
+	} else {
+		return data, err
+	}
+}
+
+func (u *Item) Delete(DB *mongo.Database) error {
+	ctx, cancel := context.WithTimeout(context.Background(), database.CTimeOut)
+	defer cancel()
+	now := time.Now()
+	u.DeletedAt = &now
+	if _, err := DB.Collection(u.CollectionName()).UpdateOne(ctx, bson.M{"_id": u.ID}, bson.M{"$set": u}, nil); err != nil {
 		return err
 	} else {
 		return nil
