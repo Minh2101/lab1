a5e9a45623e12a1e1facfacf5a6c6ebc9efea7ad
Minh <nbminh2101@gmail.com>
2023-03-15 14:35:05 +0700

    add api change status items

--- router/web.go
+++ router/web.go
@@ -10,5 +10,6 @@ func WebRouter(router *gin.RouterGroup) {
 	common := router.Group("/")
 	common.POST("/item", controllers.CreateItem)
 	common.PUT("/item/:id", controllers.UpdateItem)
+	common.POST("/change-status-items", controllers.ChangeStatusItems)
 	common.POST("/delete-items", controllers.DeleteItems)
 }
