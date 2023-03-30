d063733671273341f74d453319ad57ce8d782fae
Minh <nbminh2101@gmail.com>
2023-03-30 13:14:49 +0700

    update config DB main.go

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
 	"lab1/database"
 	"lab1/router"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	//Init mongo
-	if err := database.InitMongo(); err != nil {
+	if err := database.InitMongo(EnvMongoURI(), EnvMongoDBName()); err != nil {
 		return
 	}
 	defer database.CloseMongoDB()
@@ -18,4 +21,18 @@ func main() {
 	r := gin.New()
 	router.Router(r)
 	r.Run("0.0.0.0:8080")
+}
+func EnvMongoURI() string {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+	return os.Getenv("MONGO_URI")
+}
+func EnvMongoDBName() string {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+	return os.Getenv("DB_NAME")
 }
