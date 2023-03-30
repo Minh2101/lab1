94246306722ddd48db6518e41f404c1ab5cc8b9e
Minh <nbminh2101@gmail.com>
2023-03-30 13:13:55 +0700

    update config DB mongo.go

--- database/mongo.go
+++ database/mongo.go
@@ -3,7 +3,6 @@ package database
 import (
 	"context"
 	"fmt"
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"os"
 	"time"
@@ -16,30 +15,20 @@ var mongoDB *mongo.Database
 
 const CTimeOut = 10 * time.Second
 
-func EnvMongoURI() string {
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("Error loading .env file")
-	}
-
-	return os.Getenv("MONGO_URI")
-}
-
-func InitMongo() error {
+func InitMongo(URI, DBName string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), CTimeOut)
 	defer cancel()
 
-	clientOptions := options.Client().ApplyURI(EnvMongoURI())
+	clientOptions := options.Client().ApplyURI(URI)
 	if client, err := mongo.Connect(ctx, clientOptions); err != nil {
 		return err
 	} else {
-		fmt.Println("Mongo: Kết nối thành công")
 		err = client.Ping(ctx, readpref.Primary())
 		if err != nil {
 			fmt.Println("Ping không thành công")
 			os.Exit(1)
 		}
-		mongoDB = client.Database("myDB")
+		mongoDB = client.Database(DBName)
 		return nil
 	}
 }
