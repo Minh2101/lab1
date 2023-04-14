70070181a5888e15e6547d7abef5f97790a32067
Minh <nbminh2101@gmail.com>
2023-04-14 15:44:35 +0700

    add time now

--- controllers/time.go
+++ controllers/time.go
@@ -8,3 +8,6 @@ func ConvertTimeYYYYMMDD(input string) time.Time {
 	time, _ := time.Parse("2006-01-02", input)
 	return time
 }
+func Now() time.Time {
+	return time.Now().Add(time.Duration(+7) * time.Hour)
+}
